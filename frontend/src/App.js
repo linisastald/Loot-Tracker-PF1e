f62ad5a8c093c23a2a87e54f72baacd520f858bb
Daniel <git@danielkempson.com>
2024-07-08 18:06:51 -0400

    unprocessed loot update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,29 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Updated import for Routes
 import Login from './components/Login';
 import LootEntry from './components/LootEntry';
 import UnprocessedLoot from './components/UnprocessedLoot';
 import GoldTransactions from './components/GoldTransactions';
 import MainLayout from './components/MainLayout';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from './theme';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/login" component={Login} />
-        <MainLayout>
-          <Route path="/loot-entry" component={LootEntry} />
-          <Route path="/unprocessed-loot" component={UnprocessedLoot} />
-          <Route path="/gold-transactions" component={GoldTransactions} />
-        </MainLayout>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route element={<MainLayout />}>
+            <Route path="/loot-entry" element={<LootEntry />} />
+            <Route path="/unprocessed-loot" element={<UnprocessedLoot />} />
+            <Route path="/gold-transactions" element={<GoldTransactions />} />
+          </Route>
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
