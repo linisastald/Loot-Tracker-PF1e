859674ac4a0a78a84bd5034255f9f57849a82a29
Daniel <git@danielkempson.com>
2024-07-08 18:05:14 -0400

    unprocessed loot update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; // Use Switch instead of Routes
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import theme from './theme';
-import UnprocessedLoot from './components/UnprocessedLoot';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './components/Login';
 import LootEntry from './components/LootEntry';
+import UnprocessedLoot from './components/UnprocessedLoot';
 import GoldTransactions from './components/GoldTransactions';
+import MainLayout from './components/MainLayout';
 
-const App = () => {
+function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Router>
-        <Switch> {/* Use Switch instead of Routes */}
-          <Route path="/unprocessed-loot" component={UnprocessedLoot} />
+    <Router>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <MainLayout>
           <Route path="/loot-entry" component={LootEntry} />
+          <Route path="/unprocessed-loot" component={UnprocessedLoot} />
           <Route path="/gold-transactions" component={GoldTransactions} />
-        </Switch>
-      </Router>
-    </ThemeProvider>
+        </MainLayout>
+      </Switch>
+    </Router>
   );
-};
+}
 
 export default App;
