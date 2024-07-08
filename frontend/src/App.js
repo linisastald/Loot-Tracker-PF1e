c7dd33bf67b87146c6a0408b428965911b153ecd
Daniel <git@danielkempson.com>
2024-07-08 18:04:24 -0400

    unprocessed loot update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; // Use Switch instead of Routes
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from './theme';
 import UnprocessedLoot from './components/UnprocessedLoot';
 import LootEntry from './components/LootEntry';
 import GoldTransactions from './components/GoldTransactions';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Router>
-        <Routes>
-          <Route path="/unprocessed-loot" element={<UnprocessedLoot />} />
-          <Route path="/loot-entry" element={<LootEntry />} />
-          <Route path="/gold-transactions" element={<GoldTransactions />} />
-        </Routes>
+        <Switch> {/* Use Switch instead of Routes */}
+          <Route path="/unprocessed-loot" component={UnprocessedLoot} />
+          <Route path="/loot-entry" component={LootEntry} />
+          <Route path="/gold-transactions" component={GoldTransactions} />
+        </Switch>
       </Router>
     </ThemeProvider>
   );
