76fe0ab464d999ecb9d17a3405acd17918149c7d
Daniel <git@danielkempson.com>
2024-07-13 13:47:30 -0400

    dm settings work

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -7,14 +7,16 @@ const checkRole = require('../middleware/checkRole');
 router.put('/change-password', verifyToken, userController.changePassword);
 router.get('/characters', verifyToken, userController.getCharacters);
 router.post('/characters', verifyToken, userController.addCharacter);
-router.get('/all', verifyToken, userController.getCharacters);
+router.get('/all', verifyToken, userController.getAllUsers);
 router.put('/characters', verifyToken, userController.updateCharacter);
-router.get('/:id', verifyToken, userController.getUserById);
 router.put('/deactivate-all-characters', verifyToken, userController.deactivateAllCharacters);
 router.put('/reset-password', verifyToken, checkRole(['DM']), userController.resetPassword);
 router.put('/delete-user', verifyToken, checkRole(['DM']), userController.deleteUser);
 router.put('/update-setting', verifyToken, checkRole(['DM']), userController.updateSetting);
 router.get('/settings', verifyToken, checkRole(['DM']), userController.getSettings);
-//router.get('/all', verifyToken, userController.getAllUsers);
+//
+router.get('/:id', verifyToken, userController.getUserById);
+
+
 
 module.exports = router;
