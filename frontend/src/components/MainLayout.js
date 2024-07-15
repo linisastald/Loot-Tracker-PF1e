28662c1befce6443d4ecd4700440bc876d6d7803
Daniel <git@danielkempson.com>
2024-07-15 16:14:08 -0400

    Width fixing

--- frontend/src/components/MainLayout.js
+++ frontend/src/components/MainLayout.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Sidebar from './Sidebar';
 import { Box } from '@mui/material';
-import { Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom'; // Import Outlet to render child routes
 
 const MainLayout = () => {
   return (
-    <Box sx={{ display: 'flex', height: '100vh' }}>
+    <Box className="main-layout">
       <Sidebar />
-      <Box sx={{ flexGrow: 1, p: 3 }}>
-        <Outlet />
+      <Box className="content" sx={{ width: '90%' }}>
+        <Outlet /> {/* Render the child routes */}
       </Box>
     </Box>
   );
