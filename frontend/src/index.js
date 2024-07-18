c8d78cac4dd3ec28a1d0a6fca4d8aa8909df7713
Daniel <git@danielkempson.com>
2024-07-17 21:02:24 -0400

    Update hardcode to variable

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,6 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import App from './App';
 import './globalStyles.css';
-require('dotenv').config();
 
 
 const theme = createTheme({
