a3b9ad7b80555c50a2796c21883987b9a0e4aa2c
Daniel <git@danielkempson.com>
2024-07-20 20:21:22 -0400

    blah

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import jwt_decode from 'jwt-decode';
 
-const API_URL = process.env.API_URL;
+const API_URL = process.env.REACT_APP_API_URL;
 console.log('API_URL:', API_URL); // Add this line to check
 
 export const fetchActiveUser = async () => {
