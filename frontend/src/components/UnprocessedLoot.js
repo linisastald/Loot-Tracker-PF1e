81dec434b5bcd16e24b891e97a87a06ce30f8bb7
Daniel <git@danielkempson.com>
2024-07-21 20:34:45 -0400

    appraise v0.0000000000000000000000000000000000001

--- frontend/src/components/UnprocessedLoot.js
+++ frontend/src/components/UnprocessedLoot.js
@@ -46,7 +46,7 @@ const UnprocessedLoot = () => {
     fetchActiveUserDetails();
   }, []);
 
-  const fetchLoot = async (user.activeCharacterId) => {
+  const fetchLoot = async (activeCharacterId) => {
     try {
       const token = localStorage.getItem('token');
       const response = await axios.get(`${API_URL}/loot`, {
