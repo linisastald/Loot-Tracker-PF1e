5a3ea92615e9308f7aa57bd63fc7851e9715c689
Daniel <git@danielkempson.com>
2024-07-23 13:56:56 -0400

    Update value calculation

--- backend/services/calculateFinalValue.js
+++ backend/services/calculateFinalValue.js
@@ -1,19 +1,22 @@
 const calculateFinalValue = (itemValue, itemType, mods) => {
   let modifiedValue = itemValue;
   let totalPlus = 0;
-  console.log(modifiedValue, itemType, mods )
+  console.log("Initial item value:", modifiedValue);
+  console.log("Item type:", itemType);
+  console.log("Mods:", mods);
+
   mods.forEach(mod => {
     if (mod.valuecalc) {
       // Apply value calculation
       const valuecalc = mod.valuecalc.replace('item.wgt', '1'); // Default item weight to 1
-      console.log("modval 1:",modifiedValue)
-      console.log("valcalc 1:",valuecalc)
+      console.log("Before applying valuecalc:", modifiedValue);
+      console.log("Valuecalc to apply:", valuecalc);
       modifiedValue = eval(`${modifiedValue}${valuecalc}`);
-      console.log("modval 2:",modifiedValue)
+      console.log("After applying valuecalc:", modifiedValue);
     }
     if (mod.plus) {
       totalPlus += mod.plus;
-      console.log("total plus:",totalPlus)
+      console.log("Total plus after adding mod:", totalPlus);
     }
   });
 
@@ -22,13 +25,21 @@ const calculateFinalValue = (itemValue, itemType, mods) => {
     weapon: { 1: 2000, 2: 8000, 3: 18000, 4: 32000, 5: 50000, 6: 72000, 7: 98000, 8: 128000, 9: 162000, 10: 200000 },
     armor: { 1: 1000, 2: 4000, 3: 9000, 4: 16000, 5: 25000, 6: 36000, 7: 49000, 8: 64000, 9: 81000, 10: 100000 }
   };
-  console.log("test",plusValueTables[itemType]?.[totalPlus])
 
-  const additionalValue = plusValueTables[itemType]?.[totalPlus] || 0;
-  console.log("add val:",additionalValue);
-  console.log("modval3:",modifiedValue + additionalValue);
-  return modifiedValue + additionalValue;
+  let additionalValue;
+  if (itemType.toLowerCase() === 'weapon') {
+    additionalValue = plusValueTables.weapon[totalPlus] || 0;
+  } else if (itemType.toLowerCase() === 'armor') {
+    additionalValue = plusValueTables.armor[totalPlus] || 0;
+  } else {
+    additionalValue = 0;
+  }
 
+  console.log("Additional value based on total plus:", additionalValue);
+
+  const finalValue = modifiedValue + additionalValue;
+  console.log("Final calculated value:", finalValue);
+  return finalValue;
 };
 
 module.exports = { calculateFinalValue };
