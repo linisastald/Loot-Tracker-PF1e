88fc57e42f82d703f70c1996059bb9e20a70ffd3
Daniel <git@danielkempson.com>
2024-07-27 13:00:38 -0400

    docker start

--- start.sh
+++ start.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
 
+# Start PostgreSQL
+/usr/local/bin/docker-entrypoint.sh postgres &
+
 # Wait for PostgreSQL
 until PGPASSWORD=$DB_PASSWORD psql -h db -U $DB_USER -d $DB_NAME -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
 
->&2 echo "Postgres is up - executing command"
+# Initialize the database
+psql -U postgres -f /app/database/init.sql
 
-# Start the Node.js backend
-cd /app/backend && npm start &
+# Run the other SQL scripts
+psql -U $DB_USER -d $DB_NAME -f /app/database/item_data.sql
+psql -U $DB_USER -d $DB_NAME -f /app/database/mod_data.sql
+
+# Start the backend
+cd /app/backend
+node index.js &
 
 # Start nginx
 nginx -g 'daemon off;'
+
