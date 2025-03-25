ee01786f446b40296569a1a3e5ac3ba766923bb7
Daniel <git@danielkempson.com>
2025-03-25 02:46:03 -0400

    error correction

--- backend/src/api/routes/loot.js
+++ backend/src/api/routes/loot.js
@@ -2,14 +2,13 @@ const express = require('express');
 const router = express.Router();
 const lootController = require('../../controllers/lootController');
 const verifyToken = require('../../middleware/auth');
-const checkRole = require('../../middleware/checkRole');
 
 // Add the route to parse item description
 router.post('/parse-item', lootController.parseItemDescription);
 
 router.post('/', verifyToken, lootController.createLoot);
 router.get('/', verifyToken, lootController.getAllLoot);
-router.get('/items', verifyToken, lootController.getItems);
+router.post('/parse-item', lootController.parseItemDescription);
 router.post('/split-stack', verifyToken, lootController.splitStack);
 router.get('/trash', verifyToken, lootController.getTrashedLoot);
 router.get('/kept-party', verifyToken, lootController.getKeptPartyLoot);
@@ -18,20 +17,19 @@ router.put('/update-status', verifyToken, lootController.updateLootStatus);
 router.get('/unprocessed-count', lootController.getUnprocessedCount);
 router.get('/items-by-id', verifyToken, lootController.getItemsById);
 router.get('/mods-by-id', verifyToken, lootController.getModsById);
-router.get('/pending-sale', verifyToken, checkRole('DM'), lootController.getPendingSaleItems);
-router.get('/search', verifyToken, checkRole('DM'), lootController.searchItems);
-router.put('/confirm-sale', verifyToken, checkRole('DM'), lootController.confirmSale);
+router.get('/pending-sale', verifyToken, lootController.getPendingSaleItems);
+router.get('/search', verifyToken, lootController.searchItems);
+router.put('/confirm-sale', verifyToken, lootController.confirmSale);
 router.put('/update-entry/:id', verifyToken, lootController.updateItem);
 router.post('/appraise', lootController.appraiseLoot);
 router.post('/identify', verifyToken, lootController.identifyItems);
-router.get('/mods', verifyToken, checkRole('DM'), lootController.getMods);
-router.put('/dm-update/:id', verifyToken, checkRole(['DM']), lootController.dmUpdateItem);
+router.get('/mods', verifyToken, lootController.getMods);
+router.put('/dm-update/:id', verifyToken, lootController.dmUpdateItem);
 router.get('/character-ledger', verifyToken, lootController.getCharacterLedger);
 router.get('/unidentified', verifyToken, lootController.getUnidentifiedItems);
-router.post('/sell-up-to', verifyToken, checkRole(['DM']), lootController.sellUpTo);
-router.post('/sell-all-except', verifyToken, checkRole(['DM']), lootController.sellAllExcept);
-router.post('/sell-selected', verifyToken, checkRole(['DM']), lootController.sellSelected);
-
+router.post('/sell-up-to', verifyToken, lootController.sellUpTo);
+router.post('/sell-all-except', verifyToken, lootController.sellAllExcept);
+router.post('/sell-selected', verifyToken, lootController.sellSelected);
 
 router.put('/:id', verifyToken, lootController.updateSingleLootStatus);
 
