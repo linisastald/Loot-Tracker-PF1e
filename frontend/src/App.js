6ef72214b0f516e71ae512ab4008357bd7489375
Daniel <git@danielkempson.com>
2025-03-28 15:32:52 -0400

    update theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 // src/App.js
 import CssBaseline from '@mui/material/CssBaseline';
-import './styles/buttonOverrides.css';
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
